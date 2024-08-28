26a3c02b8a6d8894935fd675acfc3677ead0076b
Anupam <itsmrad@gmail.com>
2024-08-28 17:51:09 +0530

    Create-Next-App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function page() {
+  return (
+    <div>
+        <h1 className='text-3xl underline'>
+            Home
+        </h1>
+    </div>
+  )
+}
