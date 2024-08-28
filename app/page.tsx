e4fdb0a5f5f6643b9bbe016b0b2d3a4885247172
Anupam <itsmrad@gmail.com>
2024-08-29 01:13:19 +0530

    ShadCnUi Theme Implement

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
-  return (
-    <div>
-        <h1 className='text-3xl underline'>
-            Home
-        </h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1 className="text-3xl underline">
+				Home
+				<Button>Click Me</Button>
+			</h1>
+		</div>
+	);
 }
