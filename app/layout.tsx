4b6d3c01e7365c266f59fd4bdc5b76f7416dfa13
Anupam <itsmrad@gmail.com>
2024-09-03 20:01:07 +0530

    Implemented ShadCn Basic Form

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout({
 				)}>
 				<ThemeProvider
 					attribute="class"
-					defaultTheme="dark">
+					defaultTheme="dark"
+                >
 					{children}
 				</ThemeProvider>
 			</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { assert } from "console";
+import PatientForm from "@/components/forms/PatientForm";
+
 
 export default function page() {
 	return (
-		<div>
-			<h1 className="text-3xl underline">
-				Home
-				<Button>Click Me</Button>
-			</h1>
+		<div className="flex h-screen max-h-screen">
+            {/* Otp auth for admin */}
+			<section className="remove-scrollbar container my-auto">
+                <div className="sub-container max-w-[496px]">
+                    <Image 
+                        src="/assets/icons/logo.png"
+                        height={1000}
+                        width={1000}
+                        alt="image-logo"
+                        className="mb-12 h-10 w-fit"
+                    />
+                    <PatientForm />
+                    <div className="text-14-regular mt-20 flex justify-between">
+                    <p className="justify-items-end text-dark-600 xl: text-left">
+                        MedEase
+                    </p>
+                    <Link href="/?admin=true" className="text-green=500">
+                        Admin
+                    </Link>
+                    </div>
+                </div>
+            </section>
 		</div>
 	);
 }
