9b2cdb42fac180e8cf31d5413e8f5c1c15ad4565
Anupam <itsmrad@gmail.com>
2024-09-07 00:08:35 +0530

    ShadCnUi Form Implement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,38 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
 
 // font settings
 const fontsans = Plus_Jakarta_Sans({
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
-	variable: "--font-sans",
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
-	title: "MedEase",
-	description: "A Healthcare App for all the hospital needs",
+  title: 'MedEase',
+  description: 'A Healthcare App for all the hospital needs',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={cn(
-					"min-h-screen bg-dark-300 font-sans antialiased",
-					fontsans.variable
-				)}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-                >
-					{children}
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          'min-h-screen bg-dark-300 font-sans antialiased',
+          fontsans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,34 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { assert } from "console";
-import PatientForm from "@/components/forms/PatientForm";
-
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { assert } from 'console';
+import PatientForm from '@/components/forms/PatientForm';
 
 export default function page() {
-	return (
-		<div className="flex h-screen max-h-screen">
-            {/* Otp auth for admin */}
-			<section className="remove-scrollbar container my-auto">
-                <div className="sub-container max-w-[496px]">
-                    <Image 
-                        src="/assets/icons/logo.png"
-                        height={1000}
-                        width={1000}
-                        alt="image-logo"
-                        className="mb-12 h-10 w-fit"
-                    />
-                    <PatientForm />
-                    <div className="text-14-regular mt-20 flex justify-between">
-                    <p className="justify-items-end text-dark-600 xl: text-left">
-                        MedEase
-                    </p>
-                    <Link href="/?admin=true" className="text-green=500">
-                        Admin
-                    </Link>
-                    </div>
-                </div>
-            </section>
-		</div>
-	);
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* Otp auth for admin */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo.svg"
+            height={1000}
+            width={1000}
+            alt="image-logo"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl: text-left">
+              MedEase
+            </p>
+            <Link href="/?admin=true" className="text-green=500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
